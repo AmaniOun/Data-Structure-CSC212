d7c15ba8ac70e63a22938e11aac6755bf5921d5c
AmaniOun <amany121256@gmail.com>
2025-11-04 21:37:25 +0300

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,102 @@
+package project;
+public class Customer {
+    
+    private int customerId;
+    private String name;
+    private String email;
+    private LinkedList<Integer> orders = new LinkedList<Integer>();
+    
+    public Customer() {
+        this.customerId = 0;
+        this.name = "";
+        this.email = "";
+    }
+    
+    public Customer(int customerId, String name, String email) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+    }
+    
+    public int getCustomerId() {
+        return customerId;
+    }
+    
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    
+    public String getCusName() {
+        return name;
+    }
+    
+    public void setCusName(String name) {
+        this.name = name;
+    }
+    
+    public String getCusEmail() {
+        return email;
+    }
+    
+    public void setCusEmail(String email) {
+        this.email = email;
+    }
+    
+    public LinkedList<Integer> getOrders() {
+        return orders;
+    }
+    
+    public void addOrder(Integer orderId) {
+    if (orders.empty()) {
+        orders.insert(orderId);
+        return;
+    }
+
+    orders.findFirst();
+    
+    while (!orders.last()) {
+        orders.findNext();
+    }
+    
+    orders.insert(orderId);
+}
+    
+    public boolean removeOrder(Integer target) {
+        if (orders.empty()) {
+            return false;
+        }
+        
+        orders.findFirst();
+        while (!orders.last()) {
+            if (orders.retrieve().equals(target)) {
+                orders.remove();
+                return true;
+            }
+            orders.findNext();
+        }
+        
+        if (orders.retrieve().equals(target)) {
+            orders.remove();
+            return true;
+        }
+        
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        String str = "Customer ID: " + customerId + ", Name: " + name + ", Email: " + email;
+        
+        if (!orders.empty()) {
+            str += ", Orders: ";
+            orders.findFirst();
+            while (!orders.last()) {
+                str += orders.retrieve() + " ";
+                orders.findNext();
+            }
+            str += orders.retrieve();
+        }
+        
+        return str;
+    }
+}
