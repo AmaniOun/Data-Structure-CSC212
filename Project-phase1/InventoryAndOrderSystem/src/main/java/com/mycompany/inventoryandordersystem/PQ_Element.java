c77588feda160a75bf5a569c6e626ee8112cc6fa
AmaniOun <amany121256@gmail.com>
2025-11-27 20:03:58 +0300

    Add files via upload

---
+++ Project-phase1/InventoryAndOrderSystem/src/main/java/com/mycompany/inventoryandordersystem/PQ_Element.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.inventoryandordersystem;
+
+
+public class PQ_Element<T> {
+
+ 
+    public T data;
+    float priority;
+        
+    public PQ_Element(T e, float pr){
+                   data = e;
+                   priority = pr;
+   }
+}
+    
