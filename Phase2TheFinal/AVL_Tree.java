c48e0bad1506b4e6074175f28c1a6f8ecb4a25cb
AmaniOun <amany121256@gmail.com>
2025-11-30 00:27:46 +0300

    Add files via upload

---
+++ Phase2TheFinal/AVL_Tree.java
@@ -0,0 +1,309 @@
+
+package project;
+
+
+
+public class AVL_Tree<K extends Comparable<K>, T> {
+    
+    class AVLNode<K extends Comparable<K>, T> {
+        K key;
+        T data;
+        int height;
+        AVLNode<K, T> left, right;
+        
+        AVLNode(K k, T d) {
+            key = k;
+            data = d;
+            height = 1;
+            left = right = null;
+        }
+    }
+    
+    private AVLNode<K, T> root;
+    private AVLNode<K, T> current;
+    private int size;
+    
+    public AVL_Tree() {
+        root = current = null;
+        size = 0;
+    }
+    
+    public boolean empty() {
+        return root == null;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public T retrieve() {
+        return current.data;
+    }
+    
+    public boolean findKey(K key) {
+        AVLNode<K, T> node = findNode(root, key);
+        if (node == null)
+            return false;
+        current = node;
+        return true;
+    }
+    
+    private AVLNode<K, T> findNode(AVLNode<K, T> node, K key) {
+        if (node == null)
+            return null;
+        
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0)
+            return node;
+        else if (cmp < 0)
+            return findNode(node.left, key);
+        else
+            return findNode(node.right, key);
+    }
+    
+    public boolean insert(K key, T data) {
+        if (findKey(key))
+            return false; 
+        
+        root = insertNode(root, key, data);
+        size++;
+        return true;
+    }
+    
+    private AVLNode<K, T> insertNode(AVLNode<K, T> node, K key, T data) {
+        if (node == null) {
+            current = new AVLNode<>(key, data);
+            return current;
+        }
+        
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = insertNode(node.left, key, data);
+        else if (cmp > 0)
+            node.right = insertNode(node.right, key, data);
+        else
+            return node; 
+        
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        
+        int balance = getBalance(node);
+        
+        // Left Left Case
+        if (balance > 1 && key.compareTo(node.left.key) < 0)
+            return rotateRight(node);
+        
+        // Right Right Case
+        if (balance < -1 && key.compareTo(node.right.key) > 0)
+            return rotateLeft(node);
+        
+        // Left Right Case
+        if (balance > 1 && key.compareTo(node.left.key) > 0) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+        
+        // Right Left Case
+        if (balance < -1 && key.compareTo(node.right.key) < 0) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+        
+        return node;
+    }
+    
+    public boolean update(K key, T data) {
+        if (!findKey(key))
+            return false;
+        current.data = data;
+        return true;
+    }
+    
+    public boolean remove(K key) {
+        if (!findKey(key))
+            return false;
+        
+        root = removeNode(root, key);
+        size--;
+        return true;
+    }
+    
+    private AVLNode<K, T> removeNode(AVLNode<K, T> node, K key) {
+        if (node == null)
+            return node;
+        
+        int cmp = key.compareTo(node.key);
+        
+        if (cmp < 0)
+            node.left = removeNode(node.left, key);
+        else if (cmp > 0)
+            node.right = removeNode(node.right, key);
+        else {
+            // Node with one child or no child
+            if (node.left == null || node.right == null) {
+                AVLNode<K, T> temp = (node.left != null) ? node.left : node.right;
+                
+                if (temp == null) {
+                    temp = node;
+                    node = null;
+                } else
+                    node = temp;
+            } else {
+                // Node with two children
+                AVLNode<K, T> temp = minValueNode(node.right);
+                node.key = temp.key;
+                node.data = temp.data;
+                node.right = removeNode(node.right, temp.key);
+            }
+        }
+        
+        if (node == null)
+            return node;
+        
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        
+        int balance = getBalance(node);
+        
+        // Left Left Case
+        if (balance > 1 && getBalance(node.left) >= 0)
+            return rotateRight(node);
+        
+        // Left Right Case
+        if (balance > 1 && getBalance(node.left) < 0) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+        
+        // Right Right Case
+        if (balance < -1 && getBalance(node.right) <= 0)
+            return rotateLeft(node);
+        
+        // Right Left Case
+        if (balance < -1 && getBalance(node.right) > 0) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+        
+        return node;
+    }
+    
+    private AVLNode<K, T> minValueNode(AVLNode<K, T> node) {
+        AVLNode<K, T> current = node;
+        while (current.left != null)
+            current = current.left;
+        return current;
+    }
+    
+    private int height(AVLNode<K, T> node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+    
+    private int getBalance(AVLNode<K, T> node) {
+        if (node == null)
+            return 0;
+        return height(node.left) - height(node.right);
+    }
+    
+    private AVLNode<K, T> rotateRight(AVLNode<K, T> y) {
+        AVLNode<K, T> x = y.left;
+        AVLNode<K, T> T2 = x.right;
+        
+        x.right = y;
+        y.left = T2;
+        
+        y.height = 1 + Math.max(height(y.left), height(y.right));
+        x.height = 1 + Math.max(height(x.left), height(x.right));
+        
+        return x;
+    }
+    
+    private AVLNode<K, T> rotateLeft(AVLNode<K, T> x) {
+        AVLNode<K, T> y = x.right;
+        AVLNode<K, T> T2 = y.left;
+        
+        y.left = x;
+        x.right = T2;
+        
+        x.height = 1 + Math.max(height(x.left), height(x.right));
+        y.height = 1 + Math.max(height(y.left), height(y.right));
+        
+        return y;
+    }
+    
+    public void inOrder(Visitor<T> visitor) {
+        inOrderRec(root, visitor);
+    }
+    
+    private void inOrderRec(AVLNode<K, T> node, Visitor<T> visitor) {
+        if (node != null) {
+            inOrderRec(node.left, visitor);
+            visitor.visit(node.data);
+            inOrderRec(node.right, visitor);
+        }
+    }
+    
+    // Range query
+    public LinkedList<T> rangeSearch(K min, K max) {
+        LinkedList<T> result = new LinkedList<>();
+        rangeSearchRec(root, min, max, result);
+        return result;
+    }
+    
+    private void rangeSearchRec(AVLNode<K, T> node, K min, K max, LinkedList<T> result) {
+        if (node == null)
+            return;
+        
+        if (min.compareTo(node.key) < 0)
+            rangeSearchRec(node.left, min, max, result);
+        
+        if (min.compareTo(node.key) <= 0 && max.compareTo(node.key) >= 0) {
+            if (result.empty())
+                result.insert(node.data);
+            else {
+                result.findFirst();
+                result.insert(node.data);
+            }
+        }
+        
+        if (max.compareTo(node.key) > 0)
+            rangeSearchRec(node.right, min, max, result);
+    }
+    
+    // Get all elements as LinkedList 
+    public LinkedList<T> toLinkedList() {
+        LinkedList<T> list = new LinkedList<>();
+        toLinkedListRec(root, list);
+        return list;
+    }
+    
+    private void toLinkedListRec(AVLNode<K, T> node, LinkedList<T> list) {
+        if (node != null) {
+            toLinkedListRec(node.left, list);
+            if (list.empty())
+                list.insert(node.data);
+            else {
+                list.findFirst();
+                list.insert(node.data);
+            }
+            toLinkedListRec(node.right, list);
+        }
+    }
+    
+    public void print() {
+        printRec(root);
+    }
+    
+    private void printRec(AVLNode<K, T> node) {
+        if (node != null) {
+            printRec(node.left);
+            System.out.println(node.data);
+            printRec(node.right);
+        }
+    }
+    
+
+    public interface Visitor<T> {
+        void visit(T data);
+    }
+}
---
+++ Phase2TheFinal/Products.java
@@ -0,0 +1,413 @@
+
+package project;
+
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Products {
+    
+    public static Scanner input = new Scanner(System.in);
+    public static AVL_Tree<Integer, Products> products = new AVL_Tree<>();
+
+    public AVL_Tree<Integer, Products> getproductsData() {
+        return products;
+    }
+    
+    private int productId;
+    private String name;
+    private double price;
+    private int stock;
+    private LinkedList<Integer> reviews = new LinkedList<>();
+
+    public Products() {
+        this.productId = 0;
+        this.name = "";
+        this.price = 0;
+        this.stock = 0;
+    }
+
+    public Products(int productId, String name, double price, int stock) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return name;
+    }
+
+    public void setProductName(String name) {
+        this.name = name;
+    }
+
+    public double getProductPrice() {
+        return price;
+    }
+
+    public void setProductPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void addStock(int stock) {
+        this.stock += stock;
+    }
+    
+    public void removeStock(int stock) {
+        this.stock -= stock;
+    }
+    
+    public void addReview(Integer reviewId) {
+        reviews.insert(reviewId);
+    }
+    
+    public boolean removeReview(Integer reviewId) {
+        if (!reviews.empty()) {
+            reviews.findFirst();
+            while (!reviews.last()) {
+                if (reviews.retrieve().equals(reviewId)) {
+                    reviews.remove();
+                    return true;
+                } else
+                    reviews.findNext();
+            }
+            if (reviews.retrieve().equals(reviewId)) {
+                reviews.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public LinkedList<Integer> getReviews() {
+        return reviews;
+    }
+    
+    // Load products from file
+    public Products(String fileName) {
+        try {
+            File file = new File(fileName);
+            
+            System.out.println("Searching for: " + file.getAbsolutePath());
+            
+            if (!file.exists()) {
+                System.out.println("File NOT found: " + fileName);
+                System.out.println("Please put the file in: (\"user.dir\")");
+                return;
+            }
+            
+            System.out.println("File found! Reading...");
+            
+            Scanner reader = new Scanner(file);
+            String line = reader.nextLine(); // Skip header
+            
+            int count = 0;
+            while (reader.hasNext()) {
+                line = reader.nextLine().trim();
+                if (line.isEmpty()) continue;
+                
+                String[] data = line.split(",");
+                
+                int productId = Integer.parseInt(data[0].trim());
+                String name = data[1].trim();
+                double price = Double.parseDouble(data[2].trim());
+                int stock = Integer.parseInt(data[3].trim());
+
+                Products product = new Products(productId, name, price, stock);
+                products.insert(productId, product); 
+                count++;
+            }
+            reader.close();
+            System.out.println("Successfully loaded " + count + " products from " + fileName);
+            
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    // Add product
+    public void addProduct() {
+        try {
+            System.out.println("Please enter the product ID:");
+            int productId = input.nextInt();
+            while (checkProductID(productId)) {
+                System.out.println("This ID already exists, please try a different one:");
+                productId = input.nextInt();
+            }
+            
+            System.out.println("Enter the product name:");
+            String name = input.next();
+            
+            System.out.println("Enter the price:");
+            double price = input.nextDouble();
+            
+            System.out.println("stock:");
+            int stock = input.nextInt();
+            
+            Products product = new Products(productId, name, price, stock);
+            products.insert(productId, product); 
+            System.out.println("Product added successfully!");
+            
+        } catch (Exception e) {
+            System.out.println("Invalid input, please try again.");
+            input.nextLine();
+        }
+    }
+    
+    // Search by ID
+    public Products searchProducID() {
+        try {
+            if (products.empty()) {
+                System.out.println("The product list is currently empty");
+                return null;
+            }
+            
+            System.out.println("Please enter the product ID: ");
+            int productID = input.nextInt();
+            
+            if (products.findKey(productID)) {
+                return products.retrieve();
+            }
+            
+            System.out.println("Product not found in the system");
+            return null;
+            
+        } catch (Exception e) {
+            System.out.println("Invalid input, please try again.");
+            input.nextLine();
+            return null;
+        }
+    }
+    
+    // Remove product
+    public Products removeProduct() {
+        try {
+            if (products.empty()) {
+                System.out.println("The product list is currently empty");
+                return null;
+            }
+            
+            System.out.println("Enter the product ID to remove: ");
+            int productID = input.nextInt();
+            
+            if (products.findKey(productID)) { 
+                Products removedProduct = products.retrieve();
+                products.remove(productID); 
+                System.out.println("Product " + productID + " has been removed from the system");
+                return removedProduct;
+            }
+            
+            System.out.println("Product not found in the system");
+            return null;
+        } catch (Exception e) {
+            System.out.println("Invalid input, please try again.");
+            input.nextLine();
+            return null;
+        }
+    }
+   
+    // Update product 
+    public Products updateProduct() {
+        try {
+            if (products.empty()) {
+                System.out.println("The product list is currently empty");
+                return null;
+            }
+            
+            System.out.println("Enter the product ID to update: ");
+            int productID = input.nextInt();
+            
+            if (products.findKey(productID)) {
+                Products p = products.retrieve();
+                
+                System.out.println("1. Update product Name");
+                System.out.println("2. Update product price");
+                System.out.println("3. Update stock");
+                System.out.println("Enter your choice");
+                int choice = input.nextInt();
+                
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter new product name:");
+                        p.setProductName(input.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter new price:");
+                        p.setProductPrice(input.nextDouble());
+                        break;
+                    case 3:
+                        System.out.println("Enter new stock quantity:");
+                        int stock = input.nextInt();
+                        p.setStock(stock);
+                        break;
+                    default:
+                        System.out.println("Invalid selection");
+                }
+                products.update(productID, p);
+                return p;
+            }
+            
+            System.out.println("Product not found in the system");
+            return null;
+        } catch (Exception e) {
+            System.out.println("Invalid input, please try again.");
+            input.nextLine();
+            return null;
+        }
+    }
+
+    // Search by name
+    public Products searchProducName() {
+        try {
+            if (products.empty()) {
+                System.out.println("The product list is currently empty");
+                return null;
+            }
+            
+            System.out.println("Enter product Name to search: ");
+            String name = input.nextLine();
+            name = input.nextLine();
+            
+            final String searchName = name;
+            final Products[] result = new Products[1];
+            
+            products.inOrder(new AVL_Tree.Visitor<Products>() {
+                public void visit(Products p) {
+                    if (p.getProductName().equalsIgnoreCase(searchName)) {
+                        result[0] = p;
+                    }
+                }
+            });
+            
+            if (result[0] != null) {
+                return result[0];
+            }
+
+            System.out.println("No product found with this name");
+            return null;
+        } catch (Exception e) {
+            System.out.println("Invalid input, please try again.");
+            input.nextLine();
+            return null;
+        }
+    }
+
+    // Out of stock products
+    public void OutStockProducts() {
+        try {
+            if (products.empty()) {
+                System.out.println("The product list is currently empty");
+                return;
+            }
+            
+            final boolean[] found = {false};
+            
+            products.inOrder(new AVL_Tree.Visitor<Products>() {
+                public void visit(Products p) {
+                    if (p.getStock() == 0) {
+                        System.out.println(p);
+                        found[0] = true;
+                    }
+                }
+            });
+            
+            if (!found[0]) {
+                System.out.println("There is NO product out of stock");
+            }
+        } catch (Exception e) {
+            System.out.println("Error occurred while checking stock, please try again.");
+        }
+    }
+
+    // Check if product ID exists
+    public boolean checkProductID(int productId) {
+        try {
+            return products.findKey(productId);
+        } catch (Exception e) {
+            System.out.println("Error occurred while checking product ID, please try again.");
+            return false;
+        }
+    }
+
+    // Get product data by ID
+    public Products getProductData(int ProductId) {
+        try {
+            if (products.findKey(ProductId)) {
+                return products.retrieve();
+            }
+            return null;
+        } catch (Exception e) {
+            System.out.println("Error occurred while retrieving product data, please try again.");
+            return null;
+        }
+    }
+    
+    // Range query by price
+    public LinkedList<Products> getProductsByPriceRange(double minPrice, double maxPrice) {
+        LinkedList<Products> result = new LinkedList<>();
+        
+        products.inOrder(new AVL_Tree.Visitor<Products>() {
+            public void visit(Products p) {
+                if (p.getProductPrice() >= minPrice && p.getProductPrice() <= maxPrice) {
+                    if (result.empty())
+                        result.insert(p);
+                    else {
+                        result.findFirst();
+                        result.insert(p);
+                    }
+                }
+            }
+        });
+        
+        return result;
+    }
+    
+    // Get all products sorted by ID
+    public void printAllProductsSorted() {
+        if (products.empty()) {
+            System.out.println("No products available.");
+            return;
+        }
+        
+        System.out.println("\n--- All Products (Sorted by ID) ---");
+        products.inOrder(new AVL_Tree.Visitor<Products>() {
+            public void visit(Products p) {
+                System.out.println(p);
+            }
+        });
+    }
+    
+    @Override
+    public String toString() {
+        String str = "\nproductId=" + productId + ", name=" + name + ", price=" + price + ", stock=" + stock;
+        if (!reviews.empty()) {
+            str += " (reviews List: ";
+            reviews.findFirst();
+            while (!reviews.last()) {
+                str += reviews.retrieve() + " ";
+                reviews.findNext();
+            }
+            str += reviews.retrieve() + ")";
+        }
+        return str;
+    }
+}
